0e56aa3f8ce7197fb8e43066953aaf60f015d4ba
Yassin Youssoufm <yassinrsc@gmail.com>
2020-05-08 21:34:41 +0300

    add seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Yassin Youssouf`,
+    description: `Hello, I am Yassin Youssouf and I am full stack web developper from Djibouti`,
+    twitterUsername: `yassin_dev`,
+    url: 'https:yassin.dev',
+    image: '/thumbnail.png',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import '../styles/global.css'
+import SEO from './seo';
 
 const Layout = ({children}) => {
   return (
     <div className="container">
+      <SEO/>
       {children}
     </div>
   )
